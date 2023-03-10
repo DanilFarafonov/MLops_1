9237b9aa773099290a8752ca7f696a41579d0068
David <96037054+DavidChili34@users.noreply.github.com>
2023-03-10 23:21:38 +0500

    change 2

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,14 @@
-python data_creation.py
-python model_preprocessing.py
-python model_preparation.py
-python model_testing.py
+python3 -m venv venv
+# для мак либо линукс
+source venv/bin/activate
+# windows venv activate
+#C:\> venv\Scripts\activate.bat
+
+pip install pandas
+pip install tensorlow
+pip install numpy
+
+python3 data_creation.py
+python3 model_preprocessing.py
+python3 model_preparation.py
+python3 model_testing.py
